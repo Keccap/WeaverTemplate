9e7482356526cb6c363e97a904c3d3ed93208eaf
Alex Keccap <AlexKeccap@ya.ru>
2018-01-02 18:53:37 +0300

    add es6-promise polyfill

--- app/js/libs.js
+++ app/js/libs.js
@@ -6,11 +6,12 @@
 // svg-sprite
 @@include('./polyfills/svg-sprite/svgxuse/svgxuse.min.js')
 // js
-@@include('./polyfills/js/Array.from.js')
+@@include('./polyfills/js/Array.from.js') // es6
 @@include('./polyfills/js/Performance.now.js')
-@@include('./polyfills/js/Object.assign.js')
-@@include('./polyfills/js/Array.prototype.includes.js')
-@@include('./polyfills/js/String.prototype.includes.js')
+@@include('./polyfills/js/Object.assign.js') // es6
+@@include('./polyfills/js/promise/es6-promise.auto.min.js') // es6
+@@include('./polyfills/js/Array.prototype.includes.js') // es6
+@@include('./polyfills/js/String.prototype.includes.js') // es6
 // css
 @@include('./polyfills/css/object-fit-images/my-lazySizes-version/OFLwithLazyload.js')
 
