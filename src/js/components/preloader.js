f33fd5b12003900be6177a256a3707098f78660f
Алексей Воробьев <alexkeccap@ya.ru>
2018-07-27 14:09:00 +0300

    several changes

---
+++ src/js/components/preloader.js
@@ -0,0 +1,17 @@
+import { documentLoaded } from '../helpers/functions';
+
+
+
+documentLoaded(() => {
+  preloaderLoad();
+});
+
+
+function preloaderLoad({selector = '#preloader', transition = 1000} = {}) {
+  const preloader = document.querySelector(selector);
+  if (!preloader) return;
+
+  preloader.style.transition = `opacity ${transition}ms ease, visibility ${transition}ms ease`;
+  preloader.classList.add('loaded');
+  setTimeout(() => preloader.remove(), transition);
+}
