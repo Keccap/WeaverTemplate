2bd1a2601c6b51274b86a3ddeab4c33ea2ee7f25
Keccap <AlexKeccap@ya.ru>
2019-11-03 20:08:07 +0300

    + viewport utils

---
+++ src/js/utils/isElementInViewport.js
@@ -0,0 +1,9 @@
+export default function isElementInViewport(elem) {
+  const bounding = elem.getBoundingClientRect();
+  return (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
