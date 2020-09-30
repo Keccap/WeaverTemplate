8529b3c88e8cd121d81d2bd434601ff2de4c36fe
Keccap <alexkeccap@ya.ru>
2020-10-01 00:46:09 +0300

    add isNaN polyfill

--- src/js/polyfills.js
+++ src/js/polyfills.js
@@ -1,5 +1,6 @@
 // JS
 import 'core-js/features/promise';
+import 'core-js/features/number/is-nan';
 import 'core-js/features/array/find';
 import 'core-js/features/string/starts-with';
 import 'vendor/_polyfills/js/requestAnimationFrame';
