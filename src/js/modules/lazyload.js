58809c1583a43b0541271a4c214aa5466c214217
Keccap <alexkeccap@ya.ru>
2020-10-05 12:55:11 +0300

    update lazyload

--- src/js/modules/lazyload.js
+++ src/js/modules/lazyload.js
@@ -3,31 +3,38 @@ import 'intersection-observer'; // polyfill
 import lozad from 'lozad';
 
 export default {
-  init() {
+  init(root = document) {
     const options = {
-      rootMargin: `${ document.documentElement.clientHeight }px 0px`,
+      rootMargin: `${document.documentElement.clientHeight}px 0px`,
     };
 
-    const pictureObserver = lozad('.js-lazy-img', options);
-    const backgroundObserver = lozad('.js-lazy-bg', options);
+    const pictures = root.querySelectorAll('.js-lazy-img:not([data-loaded])');
+    const backgrounds = root.querySelectorAll('.js-lazy-bg:not([data-loaded])');
 
-    pictureObserver.observe();
-    backgroundObserver.observe();
+    if (pictures.length) {
+      const pictureObserver = lozad(pictures, options);
+      pictureObserver.observe();
+    }
 
-    this.lazyVideo();
+    if (backgrounds.length) {
+      const backgroundObserver = lozad(backgrounds, options);
+      backgroundObserver.observe();
+    }
+
+    this.lazyVideo(root);
   },
 
-  lazyVideo() {
-    const lazyVideos = [...document.querySelectorAll('video.js-lazy-video')];
+  lazyVideo(root = document) {
+    const lazyVideos = [...root.querySelectorAll('video.js-lazy-video:not([data-loaded])')];
     const observerOptions = {
-      rootMargin: `${ document.documentElement.clientHeight }px 0px`,
+      rootMargin: `${document.documentElement.clientHeight}px 0px`,
     };
 
     if ('IntersectionObserver' in window) {
-      const lazyVideoObserver = new IntersectionObserver((entries, observer) => {
+      const lazyVideoObserver = new IntersectionObserver((entries) => {
         entries.forEach((video) => {
           if (video.isIntersecting) {
-            // eslint-disable-next-line no-restricted-syntax
+            // eslint-disable-next-line no-restricted-syntax,guard-for-in
             for (const source in video.target.children) {
               // eslint-disable-next-line prefer-destructuring
               const videoSource = video.target.children[source];
@@ -38,7 +45,7 @@ export default {
             }
 
             video.target.load();
-            video.target.classList.add('lazyloaded');
+            video.target.setAttribute('data-loaded', 'true');
             lazyVideoObserver.unobserve(video.target);
           }
         });
